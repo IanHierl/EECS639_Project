8571f130c45e2ec0ee07ffbdfbb912b23c7908e8
dnewman1 <35926121+dnewman1@users.noreply.github.com>
2019-12-07 22:22:55 -0600

    Outputs all figures for all methods; refactored

--- partb.m
+++ partb.m
@@ -1,15 +1,28 @@
-n = 10000;
+n = 1000;
 h = 0.01;
+u0 = [0.2;0.0];
+t0 = 0;
+ts = linspace(t0, t0+h*n, n+1);
 
-[ xs, ts ] = EulerMethod( @Spring, [ 0.2; 0.0 ], 0, n, h )
+methods = {@EulerMethod, @BackEM, @Trapezoid, @RK4, @A4};
+figs = zeros(5,1);
+figs(1) = figure('Name', 'Euler');
+figs(2) = figure('Name', 'Backward Euler');
+figs(3) = figure('Name', 'Trapezoid');
+figs(4) = figure('Name', 'Runge-Kutta4');
+figs(5) = figure('Name', 'Adams-Bashforth/Moulton PECE');
 
-plot( xs( 1, : ), ts );
-hold on;
-f = @ ( t ) ( cos(8*t)/5 );
+f = @(t)(cos(8*t)/5);
+xs_true = f(ts);
 
-xs_true = f( ts );
+for i=1:5
+    figure(figs(i));
+    [xs,~] = methods{i}(@Spring,u0,t0,n,h);
+    hold on;
+    plot(xs(1,:),ts);
+    plot(xs_true, ts, 'r.');
+    %saveas(gcf, 'myfigure.pdf');
+    hold off;
+end
 
-plot( xs_true, ts, 'r.' );
 
-saveas( gcf, 'myfigure.pdf' );
-hold off
