218ae9bcce64311ac5b7681178fb45153554bb94
Ian Hierl <ihierl@ku.edu>
2019-12-08 17:24:19 -0600

    Update Repo

--- BZ.m
+++ BZ.m
@@ -1,6 +1,6 @@
 function [ func ] = BZ( constants )
 % u = [ a, b, x, y, z ];
-% u' = [ da/dt, db/dt, dx/dt, dy/dt, dz/dt ]
+% u' = [ da/dt, db/dt, dx/dt, dy/dt, dz/dt, dp/dt ]
 func = @(t,u) ( BZGen( t, u, constants ) );
 end
 
@@ -14,11 +14,19 @@ k3 = constants( 5 );
 k4 = constants( 6 );
 k5 = constants( 7 );
 
-uprime = zeros( 5, 1 );
-uprime( 1 ) = k4*u(3).*u(3) -k5*u(1).*u(3)-k3*u(1).*u(4);
-uprime( 2 ) = -k0*u(2).*u(5);
-uprime( 3 ) = k3*u(1).*u(4) - k2*u(3).*u(4) + k5*u(1).*u(3);
-uprime( 4 ) = -k3*u(1).*u(4) - k2*u(3).*u(4) + 0.5*f*k0*u(2).*u(5);
-uprime( 5 ) = 2*k5*u(1).*u(3) - k0*u(2).*u(5);
+A = u(1);
+B = u(2);
+X = u(3);
+Y = u(4);
+Z = u(5);
+P = u(6);
+
+uprime = zeros( 6, 1 );
+uprime( 1 ) = k4*X.*X -k5*A.*X-k3*A.*Y;
+uprime( 2 ) = -k0*B.*Z;
+uprime( 3 ) = k3*A.*Y - k2*X.*Y + k5*A.*X;
+uprime( 4 ) = -k3*A.*Y - k2*X.*Y + 0.5*f*k0*B.*Z;
+uprime( 5 ) = 2*k5*A.*X - k0*B.*Z;
+uprime( 6 ) = k3*A.*Y + 2*k2*X*Y + k4*X.*X;
 
 end
--- partc.m
+++ partc.m
@@ -1,20 +1,31 @@
-constants = [ 0.8319, 0.1343, 0.0605, 0.0842, 0.1639, 0.3242, 0.3017 ];
+close all;
+constants = 25.*[ 1; 0.001; 1; 0.1; 0.01; 0.01; 0.1; 1 ];
 
-u0 = [ 3; 5; 5; 2; 0; ];
-u0 = u0./(u0'*u0);
+% u0 = [A0; B0; X0; Y0; Z0; P0; ];
+u0 = randi( 25, 6, 1 ) + [5;5;5;5;5;5;];
+u0( 6 ) = 0;
+u0 = 0.25*u0 ./ (u0'*u0);
 
 func = BZ( constants );
 
 t0 = 0;
-tf = 50;
-n = 10000;
+tf = 250;
+n = 100000;
 h = (tf - t0)/n;
 
-[ us, ts ] = RK4( func, u0, 0, n, h );
-colors = {'k', 'y', 'r', 'g', 'b'};
+[ us, ts ] = A4( func, u0, 0, n, h );
+colors = {'k', 'y', 'r', 'g', 'b', 'cyan' };
 
+subplot( 4, 2, 1 );
 hold on;
-for i=1:5
-    plot( ts, us( i, : ), colors{i} );
-end
+for i=1:6
+    plot( ts, us( i, :), colors{i} );
+end;
+
+subplot( 4, 2, 2 );
+plot( us( :, 1 ), us( :, 5 ) );
+
+subplot( 4, 2, 3 );
+plot( us( :, 3 ), us( :, 4 ) );
+
 hold off;
