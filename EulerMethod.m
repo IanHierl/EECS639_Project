a4845f725f0dc05876b6cdcbf3b86e612578f5e6
Ian Hierl <ihierl@ku.edu>
2019-12-07 18:37:50 -0600

    Update for higher degree difeqs

--- EulerMethod.m
+++ EulerMethod.m
@@ -1,6 +1,7 @@
 function [ ys, ts ] = EulerMethod( f, y_0, t_0, n, h )
-ys = zeros( 1, n+1 );
-ys( 1 ) = y_0;
+deg = length( y_0 );
+ys = zeros( deg, n+1 );
+ys( :, 1 ) = y_0;
 ts = linspace( t_0, t_0 + h*n, n + 1 );
 
 for k = 2:n+1
